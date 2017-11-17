24dec9e8f2e29e8991dc3634dea2514529716a84
liuchuan <xyz8279635@163.com>
2017-11-17 07:37:21 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,13 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>Home</h1>
+    <div class="jumbotron">
+        <h1>Hello Sample.app</h1>
+        <p class="lead">
+            你现在看到的是<a href="#">Laravel</a>的示例项目主页。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
