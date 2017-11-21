d85cb43cc0e65b30994ef2322e46ef22e79dcd84
liuchuan <xyz8279635@163.com>
2017-11-21 11:15:39 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -107,6 +107,20 @@ class UsersController extends Controller
         return back();
     }
 
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
     protected function sendEmailConfirmationTo($user)
     {
         $view = 'emails.confirm';
